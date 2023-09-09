48a0660184d3805e42641d6a58c4a0dec887baa4
haic1947 <haic1947@gmail.com>
2023-09-10 04:45:41 +0700

    change

---
+++ dslk/node2.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+using namespace std;
+
+typedef struct tagNode {
+    int info;
+    struct tagNode* pNext;
+} Node;
+
+typedef Node* PtrNode;
+
+void init(PtrNode& pHead) {
+    pHead = NULL;
+}
+
+bool isEmpty(PtrNode pHead) {
+    return pHead == NULL;
+}
+
+PtrNode createNode(int x) {
+    PtrNode p = new Node;
+    p->info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+PtrNode nodeLast(PtrNode pHead) {
+    PtrNode p = pHead;
+    while (p != NULL && p->pNext != NULL) {
+        p = p->pNext;
+    }
+    return p;
+}
+
+void insertLast(PtrNode& pHead, PtrNode pNew) {
+    if (isEmpty(pHead)) {
+        pHead = pNew;
+    } else {
+        PtrNode p = nodeLast(pHead);
+        p->pNext = pNew;
+    }
+}
+
+void insertFirst(PtrNode& pHead, PtrNode pNew) {
+    if (isEmpty(pHead)) {
+        pHead = pNew;
+    } else {
+        pNew->pNext = pHead;
+        pHead = pNew;
+    }
+}
+
+void input(PtrNode& pHead) {
+    int x;
+    init(pHead);
+    do {
+        cout << "Nhap gia tri (Nhap 0 de ket thuc): ";
+        cin >> x;
+        if (x == 0)
+            break;
+        PtrNode pNew = createNode(x);
+        insertFirst(pHead, pNew);
+    } while (true);
+}
+
+void output(PtrNode pHead) {
+    PtrNode p = pHead;
+    while (p != NULL) {
+        cout << p->info << "->";
+        p = p->pNext;
+    }
+    cout << "[NULL]\n";
+}
+void hoanVi(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+PtrNode searchX(PtrNode pHead, int x) {
+    PtrNode p = pHead;
+    while(p->info != x && p!=NULL)  p = p->pNext;
+    return p;
+}
+
+void DeleteNode(PtrNode& pHead, PtrNode p) {
+    if (!pHead || !p) {
+        return;
+    }
+
+    // Bước 1: Nếu danh sách chỉ có 1 phần tử và đó là nút cần xóa (p == pHead)
+    if (pHead == p) {
+        pHead = pHead->pNext;
+        delete p;
+        return;
+    }
+
+    PtrNode pTruoc = NULL;
+    PtrNode current = pHead;
+
+    while (current != NULL && current != p) {
+        pTruoc = current;
+        current = current->pNext;
+    }
+
+    // Bước 2: Kiểm tra nếu đã tìm thấy nút cần xóa (p)
+    if (current == p) {
+        pTruoc->pNext = p->pNext;
+        delete p;
+    }
+}
+
+void insertNodeAtPosition(PtrNode& pHead, int x, int position) {
+    if (position == 0 || isEmpty(pHead)) {
+        PtrNode pNew = createNode(x);
+        pNew->pNext = pHead;
+        pHead = pNew;
+    } else {
+        PtrNode pPrev = NULL;
+        PtrNode pCurrent = pHead;
+        int currentPosition = 0;
+
+        while (pCurrent != NULL && currentPosition < position) {
+            pPrev = pCurrent;
+            pCurrent = pCurrent->pNext;
+            currentPosition++;
+        }
+
+        PtrNode pNew = createNode(x);
+        pPrev->pNext = pNew;
+        pNew->pNext = pCurrent;
+    }
+}
+
+PtrNode Min(PtrNode pHead) {
+    PtrNode nodeMin = pHead;
+    PtrNode p = pHead->pNext;
+    while (p != NULL) {
+        if (nodeMin->info > p->info) 
+            nodeMin = p;       
+        p = p->pNext;
+    }
+    return nodeMin;
+}
+
+void Sort(PtrNode pHead) {
+    PtrNode p = pHead;
+    while (p != NULL) {
+        PtrNode pMin = Min(p);
+        hoanVi(p->info, pMin->info);
+        p = p->pNext;
+    }
+}
+
+int main() {
+    PtrNode pHead = NULL;
+
+    input(pHead);
+
+    cout << "Danh sach lien ket truoc khi sap xep: ";
+    output(pHead);
+
+    int valueToDelete = 3;
+    PtrNode nodeToDelete = searchX(pHead, valueToDelete); // 
+    if (nodeToDelete != NULL) {
+        // Gọi hàm DeleteNode để xóa nút
+        DeleteNode(pHead, nodeToDelete);
+        cout << "Danh sach lien ket sau khi xoa: ";
+        // Gọi hàm output để in ra danh sách sau khi xóa
+        output(pHead);
+    } else {
+        cout << "Khong tim thay node can xoa." << endl;
+    }
+
+    int x,position;
+    cout << "\nNhap gia tri can chen: ";
+    cin >> x;
+    cout << "Nhap vi tri can chen (tu 0 tro len): ";
+    cin >> position;
+
+    insertNodeAtPosition(pHead, x, position);
+
+    cout << "Danh sach lien ket sau khi chen: ";
+    output(pHead);
+
+    // Sắp xếp
+    Sort(pHead);
+    cout << "Danh sach lien ket sau khi sap xep: ";
+    output(pHead);
+
+    // Giải phóng bộ nhớ
+    while (pHead != NULL) {
+        PtrNode temp = pHead;
+        pHead = pHead->pNext;
+        delete temp;
+    }
+
+    return 0;
+}
