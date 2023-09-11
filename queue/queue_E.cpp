53534cb63123b90b686f979b90d9483e4b6ca9f0
haic1947 <haic1947@gmail.com>
2023-09-12 03:45:15 +0700

    queue

--- queue/queue_E.cpp
+++ queue/queue_E.cpp
@@ -30,12 +30,48 @@ int enQueue(Queue &q, int x) {
     }
 } 
 
-int deQueue(Queue &q, int &x) {
-    if(IsEmpty(q)) return 0; //Empty Queue
-    else {
-        x = q.node[q.front];
-        if(q.front == q.rear) Init(q);
-        else    q.front = (q.front+1) % MAXQUEUE;
-        return 1;
+// In 5 giá trị đầu trong hàng đợi
+void PrintQueue(Queue &q) {
+    if (IsEmpty(q)) {
+        cout << "Hien tai hang doi dang rong." << endl;
+        return;
     }
+
+    int current = q.front;
+    cout << "Cac gia tri trong hang doi:" << endl;
+    while (current != 4) {
+        cout << q.node[current] << " ";
+        current = (current + 1) % MAXQUEUE;
+    }
+    cout << q.node[current] << endl;
+}
+
+
+
+int main() {
+    Queue q;
+    Init(q); // Khởi tạo hàng đợi
+
+    int n;
+    cout << "\nNhap so luong phan tu hang doi: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (enQueue(q, i)) {
+            cout << "Enqueued: " << i << endl;
+        } else {
+            cout << "Queue is full, unable to enqueue: " << i << endl;
+        }
+    }
+
+    cout << "\n5 gia tri dau trong hang doi: ";
+    PrintQueue(q);
+
+    if (!IsEmpty(q)) {
+        cout << "Gia tri dau cua Queue: " << q.node[q.front] << endl;
+        cout << "Gia tri cuoi cua Queue: " << q.node[q.rear] << endl;
+    } else {
+        cout << "Queue hien tai dang rong, khong co gia tri dau hoac cuoi." << endl;
+    }
+
+    return 0;
 }
